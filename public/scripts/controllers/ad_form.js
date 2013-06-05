96a3a2ecdff704b8f0ad7d829edce5aa8bce0d0d
Filip Zrůst <filip.zrust@adato.cz>
2013-06-06 01:53:57 +0200

    AdFromCtrl reformatted
    Hey, 2 spaces indentation.

--- public/scripts/controllers/ad_form.js
+++ public/scripts/controllers/ad_form.js
@@ -21,93 +21,93 @@ License along with SOS Pomoc application.  If not, see
 angular.module('sosPomocApp')
   .controller('AdFormCtrl', function ($scope, Ad, adsService) {
 
-   $scope.findMe = function() {
-        if ($scope.geolocationAvailable) {
-            return navigator.geolocation.getCurrentPosition(function(position) {
-                return $scope.$apply(function() {
-                    return $scope.newItem.location = {
-                        lat: position.coords.latitude,
-                        lon: position.coords.longitude
-                    };
-                });
-            }, function() {});
-        }
+    $scope.findMe = function() {
+      if ($scope.geolocationAvailable) {
+        return navigator.geolocation.getCurrentPosition(function(position) {
+          return $scope.$apply(function() {
+            return $scope.newItem.location = {
+              lat: position.coords.latitude,
+              lon: position.coords.longitude
+            };
+          });
+        }, function() {});
+      }
     };
 
     $scope.initAutocomplete = function() {
-        var autocomplete, input;
-        input = document.getElementById('newLocation');
-        autocomplete = new google.maps.places.Autocomplete(input);
-        google.maps.event.addDomListener(input, 'keydown', function(e) {
-            if (e.keyCode === 13) {
-                return e.preventDefault();
-            }
-        });
-        return google.maps.event.addListener(autocomplete, 'place_changed', function() {
-            var place;
-            place = autocomplete.getPlace();
-            if (!place.geometry) {
-                return;
-            }
-            if (place.geometry.location) {
-                return $scope.$apply(function() {
-                    return $scope.newItem.location = {
-                        lat: place.geometry.location.jb,
-                        lon: place.geometry.location.kb,
-                        name: document.getElementById('newLocation').value
-                    };
-                });
-            }
-        });
+      var autocomplete, input;
+      input = document.getElementById('newLocation');
+      autocomplete = new google.maps.places.Autocomplete(input);
+      google.maps.event.addDomListener(input, 'keydown', function(e) {
+        if (e.keyCode === 13) {
+          return e.preventDefault();
+        }
+      });
+      return google.maps.event.addListener(autocomplete, 'place_changed', function() {
+        var place;
+        place = autocomplete.getPlace();
+        if (!place.geometry) {
+          return;
+        }
+        if (place.geometry.location) {
+          return $scope.$apply(function() {
+            return $scope.newItem.location = {
+              lat: place.geometry.location.jb,
+              lon: place.geometry.location.kb,
+              name: document.getElementById('newLocation').value
+            };
+          });
+        }
+      });
     };
 
     $scope.init = function() {
-        $scope.errors = []
-        $scope.formSent = false
-        $scope.newItem = {}
-        $scope.asked = false
-        $scope.showForm = false
-        $scope.allCategories = adsService.categories.map(function(category) {
-            category.checked = false;
-            return category;
-        });
-        $scope.newItem.categories = angular.copy($scope.allCategories);
-        $scope.initAutocomplete()
+      $scope.errors = []
+      $scope.formSent = false
+      $scope.newItem = {}
+      $scope.asked = false
+      $scope.showForm = false
+      $scope.allCategories = adsService.categories.map(function(category) {
+        category.checked = false;
+        return category;
+      });
+      $scope.newItem.categories = angular.copy($scope.allCategories);
+      $scope.initAutocomplete()
     }
 
     $scope.$watch('showForm', function() {
-        if($scope.showForm && !$scope.newItem.location) {
-            $scope.geolocationAvailable = navigator.geolocation ? true : false
-            $scope.asked = true
-            $scope.findMe()
-        }
+      if($scope.showForm && !$scope.newItem.location) {
+        $scope.geolocationAvailable = navigator.geolocation ? true : false
+        $scope.asked = true
+        $scope.findMe()
+      }
     });
 
     $scope.submit = function() {
-        if(!$scope.createForm.$invalid) {
-            $scope.errors = []
-            if(!$scope.newItem.location) {
-                $scope.errors.push({param: 'location', msg: "Lokalita je povinná položka."});
+      if(!$scope.createForm.$invalid) {
+        $scope.errors = []
+        if(!$scope.newItem.location) {
+          $scope.errors.push({param: 'location', msg: "Lokalita je povinná položka."});
+        }
+        else {
+          $scope.newItem.categories = $scope.newItem.categories.filter(function (el, idx, arr) { return el.checked == true; })
+          Ad.create($scope.newItem, (function(data){
+            if(data.errors) {
+              $scope.errors.push("Odeslání se nezdařilo")
             }
             else {
-                $scope.newItem.categories = $scope.newItem.categories.filter(function (el, idx, arr) { return el.checked == true; })
-                Ad.create($scope.newItem, (function(data){
-                    if(data.errors) {
-                        $scope.errors.push("Odeslání se nezdařilo")
-                    }
-                    else {
-                        $scope.errors = [];
-                        $scope.formSent = true;
-                        $scope.newItem = {}
-                        $scope.newItem.categories = $scope.allCategories
-                    }
-                }))
+              $scope.errors = [];
+              $scope.formSent = true;
+              $scope.newItem = {}
+              $scope.newItem.categories = $scope.allCategories
             }
+          }))
         }
+      }
     }
 
     $scope.toggleCategory = function(index) {
-        $scope.newItem.categories[index].checked = !$scope.newItem.categories[index].checked
+      $scope.newItem.categories[index].checked = !$scope.newItem.categories[index].checked
     }
 
     $scope.open = function() {
