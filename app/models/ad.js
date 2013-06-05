fc43e99e34e2ddf7ffcd6f43adf0b261c6ecd21b
Jiri Balaty <jiri.balaty@gmail.com>
2013-06-05 16:39:59 +0200

    Basic Ad validation

--- app/models/ad.js
+++ app/models/ad.js
@@ -1,32 +1,35 @@
 /*
-This file is part of SOS Pomoc application.
+ This file is part of SOS Pomoc application.
 
-SOS Pomoc application is free software: you can redistribute it and/or
-modify it under the terms of the GNU Affero General Public License as
-published by the Free Software Foundation, either version 3 of the
-License, or (at your option) any later version.
+ SOS Pomoc application is free software: you can redistribute it and/or
+ modify it under the terms of the GNU Affero General Public License as
+ published by the Free Software Foundation, either version 3 of the
+ License, or (at your option) any later version.
 
-SOS Pomoc application is distributed in the hope that it will be
-useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-Affero General Public License for more details.
+ SOS Pomoc application is distributed in the hope that it will be
+ useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Affero General Public License for more details.
 
-You should have received a copy of the GNU Affero General Public
-License along with SOS Pomoc application.  If not, see
-<http://www.gnu.org/licenses/>.
-*/
+ You should have received a copy of the GNU Affero General Public
+ License along with SOS Pomoc application.  If not, see
+ <http://www.gnu.org/licenses/>.
+ */
+
+var validate = require('mongoose-validator').validate;
 
 var schema = require('mongoose').Schema({
-  name: String,
-  email: String,
-  categories: Array,
-  description: String,
-  location: {
-    place: String,
-    lon: Number,
-    lat: Number
-  },
-  token: String
+    name: {type: String, required: true},
+    email: {type: String, validate: validate('isEmail')},
+    categories: Array,
+    description: String,
+    location: {
+        place: {type: String, required: true},
+        lon: Number,
+        lat: Number
+    },
+    token: String
 });
+
 
 module.exports = require('mongoose').model('Ad', schema);
